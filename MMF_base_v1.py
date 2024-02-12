d0976d452dd50f29fdb69a4a028a1cef217f7ff0
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2024-02-13 10:04:23 +1300

    Adding first version of base

---
+++ MMF_base_v1.py
@@ -0,0 +1,63 @@
+# Global Variables/Constants Go Here
+y_n_list = ["yes", "no"]
+payment_method_list = ["cash", "credit"]
+
+MAX_TICKETS = 3
+tickets_sold = 0
+
+# Functions Go Here
+def string_checker(question, valid_list, error, first_letter):
+    while True:
+        # Ask user for choice (and force lowercase)
+        response = input(question).lower()
+        
+        # Runs through list and if respose is an item in list (or first letter the full name is returned)
+        for item in valid_list:
+            # If 'first_letter' is set to yes then check the list for first letter and full strings
+            if first_letter == "y":
+                if response == item[0] or response == item:
+                    return response
+            # If 'first_letter' is set to no then only check the list for full strings
+            elif first_letter == "n":
+                if response == item:
+                    return response
+
+        # Output error if response not in list        
+        print(error)
+        print()
+
+def instructions():
+    print("Instructions go Here")
+    print()
+
+def not_blank(question):
+    while True:
+        response = input(question)
+        # Checks if response is blank and if not then continues
+        if response == "":
+            print("Sorry this can't be blank Please try again")
+        else:
+            return response
+
+# Main Routine Goes Here
+# Ask user of they want to see instructions
+played_before = string_checker("Do you want to see the instructions? ", y_n_list, "Please Choose Yes/No.", "y")
+
+if played_before == "yes":
+    instructions()
+
+# Loop to sell tickets
+while tickets_sold < MAX_TICKETS:
+    not_blank("Please enter your name or 'xxx' to quit: ")
+
+    if not_blank == "xxx":
+        break
+
+    tickets_sold += 1
+
+# Output number of ticket sold
+if tickets_sold == MAX_TICKETS:
+    print("Congrats you have sold all the tickets!")
+
+else:
+    print(f"You have sold {tickets_sold} ticket/s. You have {MAX_TICKETS-tickets_sold} ticket/s remaining.")
